8753a72bfdddcabf1a16e5cf6118574f39eabef0
msr-rana <61286387+msr-rana@users.noreply.github.com>
2021-09-08 22:04:43 +0600

    react wedgets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const items =[
+    {
+        title : 'What is React?',
+        content : 'React is front end javaScript frameWork'
+    },
+    {
+        title : 'Why use React?',
+        content : 'React is a favorite JS Library amon engineers'
+    },
+    {
+        title : 'How do you use React?',
+        content : 'you use react for create component'
+    },
+    {
+        title : 'What is React',
+        content : 'React is front end javaScript frameWork'
+    }
+],
+
+ App= ()=>{
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    );
 }
-
 export default App;
